a6d5653991f47c8e94bd0098fbc18ebaf7416336
BinaryConvertz <ryangregoire286@gmail.com>
2026-02-22 19:01:34 -0500

    added if check

--- src/main.rs
+++ src/main.rs
@@ -19,102 +19,117 @@ fn div_nums(n1: i32, n2: i32) -> i32 {
     n1 / n2
 }
 
+#[allow(dead_code)]
+
+struct InputUser {
+    value: String,
+    length_by_user: f32,
+}
+
 fn main() -> std::io::Result<()> {
-    let mut input = String::new();
-    let message: String = String::from("Enter Command: ");
-    let mut guess_input = String::new();
+    let c: InputUser = InputUser {
+        value: String::from("Hello"),
+        length_by_user: (1024 * 1024) as f32,
+    };
 
-    println!("{}", get_command_upper(message));
-    stdin().read_line(&mut input).unwrap();
+    if c.length_by_user >= 4000 as f32 {
+        let mut input = String::new();
+        let message: String = String::from("Enter Command: ");
+        let mut guess_input = String::new();
 
-    match input.trim() {
-        "/Guess" => {
-            let guess = get_random_guess(5) as i32;
+        println!("{}", get_command_upper(message));
+        stdin().read_line(&mut input).unwrap();
 
-            println!("{}", "Enter Guess Between 1 and 5: ");
-            stdin().read_line(&mut guess_input).unwrap();
-            let num_guess = guess_input.trim().parse::<i32>().expect("No Input");
+        match input.trim() {
+            "/Guess" => {
+                let guess = get_random_guess(5) as i32;
 
-            if num_guess == guess {
-                println!("{}", "Correct The Guessed Number");
-            } else if num_guess != guess {
-                let file = File::create("Guess.txt")?;
+                println!("{}", "Enter Guess Between 1 and 5: ");
+                stdin().read_line(&mut guess_input).unwrap();
+                let num_guess = guess_input.trim().parse::<i32>().expect("No Input");
 
-                let mut buff = BufWriter::new(file);
-                writeln!(buff, "{}", guess)?;
-                println!("Correct Guess was: {} ", guess);
+                if num_guess == guess {
+                    println!("{}", "Correct The Guessed Number");
+                } else if num_guess != guess {
+                    let file = File::create("Guess.txt")?;
+
+                    let mut buff = BufWriter::new(file);
+                    writeln!(buff, "{}", guess)?;
+                    println!("Correct Guess was: {} ", guess);
+                }
             }
+
+            "/Math" => {
+                let mut option = String::new();
+
+                println!("{}", "Enter Option: ");
+                stdin().read_line(&mut option).unwrap();
+                let is_add: bool = option == "+";
+                let is_sub: bool = option == "-";
+                let is_mul: bool = option == "*";
+                let is_div: bool = option == "/";
+
+                if is_add {
+                    let num_1 = get_random_guess(5) as i32;
+                    let num_2 = get_random_guess(10) as i32;
+
+                    let mut answer = String::new();
+                    stdin().read_line(&mut answer).unwrap();
+
+                    let num_answer = answer.trim().parse::<i32>().unwrap();
+
+                    if num_answer == add_nums(num_1, num_2) {
+                        println!("{}", "You are | Correct Answer")
+                    }
+                }
+
+                if is_sub {
+                    let num_1 = get_random_guess(5) as i32;
+                    let num_2 = get_random_guess(10) as i32;
+
+                    let mut answer = String::new();
+                    stdin().read_line(&mut answer).unwrap();
+
+                    let num_answer = answer.trim().parse::<i32>().unwrap();
+
+                    if num_answer == sub_nums(num_1, num_2) {
+                        println!("{}", "You are | Correct Answer")
+                    }
+                }
+
+                if is_mul {
+                    let num_1 = get_random_guess(5) as i32;
+                    let num_2 = get_random_guess(10) as i32;
+
+                    let mut answer = String::new();
+                    stdin().read_line(&mut answer).unwrap();
+
+                    let num_answer = answer.trim().parse::<i32>().unwrap();
+
+                    if num_answer == mul_nums(num_1, num_2) {
+                        println!("{}", "You are | Correct Answer")
+                    }
+                }
+
+                if is_div {
+                    let num_1 = get_random_guess(5) as i32;
+                    let num_2 = get_random_guess(10) as i32;
+
+                    let mut answer = String::new();
+                    stdin().read_line(&mut answer).unwrap();
+
+                    let num_answer = answer.trim().parse::<i32>().unwrap();
+
+                    if num_answer == div_nums(num_1, num_2) {
+                        println!("{}", "You are | Correct Answer")
+                    }
+                }
+            }
+
+            _ => println!("{}", "Default Value"),
         }
-
-        "/Math" => {
-            let mut option = String::new();
-
-            println!("{}", "Enter Option: ");
-            stdin().read_line(&mut option).unwrap();
-            let is_add: bool = option == "+";
-            let is_sub: bool = option == "-";
-            let is_mul: bool = option == "*";
-            let is_div: bool = option == "/";
-
-            if is_add {
-                let num_1 = get_random_guess(5) as i32;
-                let num_2 = get_random_guess(10) as i32;
-
-                let mut answer = String::new();
-                stdin().read_line(&mut answer).unwrap();
-
-                let num_answer = answer.trim().parse::<i32>().unwrap();
-
-                if num_answer == add_nums(num_1, num_2) {
-                    println!("{}", "You are | Correct Answer")
-                }
-            }
-
-            if is_sub {
-                let num_1 = get_random_guess(5) as i32;
-                let num_2 = get_random_guess(10) as i32;
-
-                let mut answer = String::new();
-                stdin().read_line(&mut answer).unwrap();
-
-                let num_answer = answer.trim().parse::<i32>().unwrap();
-
-                if num_answer == sub_nums(num_1, num_2) {
-                    println!("{}", "You are | Correct Answer")
-                }
-            }
-
-            if is_mul {
-                let num_1 = get_random_guess(5) as i32;
-                let num_2 = get_random_guess(10) as i32;
-
-                let mut answer = String::new();
-                stdin().read_line(&mut answer).unwrap();
-
-                let num_answer = answer.trim().parse::<i32>().unwrap();
-
-                if num_answer == mul_nums(num_1, num_2) {
-                    println!("{}", "You are | Correct Answer")
-                }
-            }
-
-            if is_div {
-                let num_1 = get_random_guess(5) as i32;
-                let num_2 = get_random_guess(10) as i32;
-
-                let mut answer = String::new();
-                stdin().read_line(&mut answer).unwrap();
-
-                let num_answer = answer.trim().parse::<i32>().unwrap();
-
-                if num_answer == div_nums(num_1, num_2) {
-                    println!("{}", "You are | Correct Answer")
-                }
-            }
-        }
-
-        _ => println!("{}", "Default Value"),
     }
+
     Ok(())
 }
 
